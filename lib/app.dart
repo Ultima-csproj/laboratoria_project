fc9c0602f475f08d48d54d6b6549b69d25224a4e
Ultima <cingizminkenov873@gmail.com>
2026-03-27 17:11:50 +0600

    chore: init flutter project

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RandomCoffee extends StatelessWidget {
+  const RandomCoffee({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:random_coffee/app.dart';
+
+void main() {
+  runApp(const RandomCoffee());
+}
