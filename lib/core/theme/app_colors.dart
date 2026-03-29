cddceb99deeeab86ae7ccc7014182d98816aa67d
Ultima <cingizminkenov873@gmail.com>
2026-03-29 14:28:45 +0600

    feat: update colors and theme, and add Open Sans font

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const primary = Color(0xFF5cbce5);
 
-  static const bgLight = Color(0xFFffffff);
-  static const surfaceLight = Color(0xFFf7faf8);
+  static const primary = Color(0xFF5CBCE5);
+  static const primaryDark = Color(0xFF269DD1);
+  static const primaryLight = Color(0xFF8BCFEE);
+
+
+  static const bgLight = Color(0xFFF7FAF8);
+  static const surfaceLight = Color(0xFFFFFFFF);
   static const textLight = Color(0xFF484647);
+  static const neutral2Light = Color(0xFFD9D9D9);
+  static const neutral3 = Color(0xFFAEAAAB);
+
 
   static const bgDark = Color(0xFF313031);
   static const surfaceDark = Color(0xFF484647);
-  static const textDark = Color(0xFFffffff);
+  static const textDark = Color(0xFFFFFFFF);
+  static const neutral2Dark = Color(0xFF797677);
 }
--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,28 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'app_colors.dart';
 
 class AppTheme {
-  static ThemeData light() => ThemeData(
-    useMaterial3: true,
-    brightness: Brightness.light,
-    primaryColor: AppColors.primary,
-    scaffoldBackgroundColor: AppColors.bgLight,
-    colorScheme: const ColorScheme.light(
-      primary: AppColors.primary,
-      surface: AppColors.surfaceLight,
-      onSurface: AppColors.textLight,
-    )
-  );
+  static ThemeData light() {
+    final base = GoogleFonts.openSansTextTheme(ThemeData.light().textTheme);
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.light,
+      primaryColor: AppColors.primary,
+      scaffoldBackgroundColor: AppColors.bgLight,
+      textTheme: base,
+      colorScheme: const ColorScheme.light(
+        primary: AppColors.primary,
+        surface: AppColors.surfaceLight,
+        onSurface: AppColors.textLight,
+        outline: AppColors.neutral2Light,
+      ),
+    );
+  }
 
-  static ThemeData dark() => ThemeData(
+  static ThemeData dark() {
+    final base = GoogleFonts.openSansTextTheme(ThemeData.dark().textTheme);
+    return ThemeData(
       useMaterial3: true,
       brightness: Brightness.dark,
       primaryColor: AppColors.primary,
       scaffoldBackgroundColor: AppColors.bgDark,
+      textTheme: base,
       colorScheme: const ColorScheme.dark(
         primary: AppColors.primary,
         surface: AppColors.surfaceDark,
         onSurface: AppColors.textDark,
-      )
-  );
+        outline: AppColors.neutral2Dark,
+      ),
+    );
+  }
 }
